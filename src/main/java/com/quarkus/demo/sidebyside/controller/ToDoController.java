74c740702a9d65a8ebaa6ea11c7ea6b57f6f4980
Naveen Kendyala <naveenkendyala@kendyala.com>
2020-01-17 16:34:31 -0500

    Updated property files and the deployments files

--- src/main/java/com/quarkus/demo/sidebyside/controller/ToDoController.java
+++ src/main/java/com/quarkus/demo/sidebyside/controller/ToDoController.java
@@ -2,11 +2,15 @@ package com.quarkus.demo.sidebyside.controller;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.agroal.api.AgroalDataSource;
 
 import com.quarkus.demo.sidebyside.entity.ToDo;
 import com.quarkus.demo.sidebyside.repository.ToDoRepository;
@@ -16,11 +20,23 @@ public class ToDoController {
 
 	@Autowired
 	private ToDoRepository todoRepository;	
-	
+
+	@Inject
+	AgroalDataSource defaultDataSource;
 
 	@GetMapping("/todos")
 	public List<ToDo> getAllToDos() {
 		return todoRepository.findAll();
+	}
+
+	@GetMapping("/applive")
+	public String liveCheck() {
+		return "OK";
+	}
+
+	@GetMapping("/appready")
+	public String healthCheck() {
+		return "OK";
 	}
 
 	@PutMapping("/todos")
